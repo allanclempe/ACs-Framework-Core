e381516b25ab3d931143f3de0bf1b80425d0f3f4
Allan Clempe <allan.clempe@gmail.com>
2016-12-03 12:11:44 +1000

    Version adjustments.

---
+++ test/ACs.Net.Mail.Tests/HtmlMessageTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ACs.Net.Mail.Message;
+using Xunit;
+
+namespace ACs.Net.Mail.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class HtmlMessageTest
+    {
+        private const string _templateHtml = "<div>Welcome <param name=\"name\" /><br><br>To activate your account click link above.<br><br><param name=\"link\" /><br><br>Thanks</div>";
+        private const string _templateHtmlWithMetas = "<meta name=\"mailto\" value=\"email@domain.com\"><meta name=\"subject\" value=\"Activate your account\"><div>Welcome <param name=\"name\" /><br><br>To activate your account click link above.<br><br><param name=\"link\" /><br><br>Thanks</div>";
+        private const string _templateHtmlWithBody = "<html><head><meta name=\"mailto\" value=\"email@domain.com\"><meta name=\"subject\" value=\"Activate your account\"></head><body><div>Welcome <param name=\"name\" /><br><br>To activate your account click link above.<br><br><param name=\"link\" /><br><br>Thanks</div></body></html>";
+
+        [Fact]
+        public void CreateBody()
+        {
+            var name = "User Name";
+            var uri = new Uri("Http://somedomain.com");
+
+            var body = new HtmlMessage(_templateHtml)
+                .SetParams(new
+                {
+                    name = name,
+                    link = uri
+                });
+
+
+            var result = body.ToHtml();
+            Assert.StartsWith("<!DOCTYPE", result);
+            Assert.Contains("<br>", result, StringComparison.InvariantCultureIgnoreCase);
+            Assert.Contains(name, result);
+            Assert.Contains("a href", result);
+            Assert.Contains(uri.ToString(), result);
+
+        }
+
+        [Fact]
+        public void SetFontFamilyAndSize()
+        {
+            var result = new HtmlMessage(_templateHtml)
+                .SetFont("arial", 12)
+                .ToHtml();
+
+            Assert.Contains("font-family:'arial'", result, StringComparison.InvariantCultureIgnoreCase);
+            Assert.Contains("font-size:12", result, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Fact]
+        public void SetMetaTags()
+        {
+            var body = new HtmlMessage(_templateHtmlWithMetas);
+
+            Assert.Equal("email@domain.com", body.MailTo);
+            Assert.Equal("Activate your account", body.Subject);
+
+        }
+
+        [Fact]
+        public void VerifyIfBodyIsReplacingByDefaultTempalte()
+        {
+            var body = new HtmlMessage(_templateHtmlWithBody);
+
+            Assert.StartsWith("<!DOCTYPE", body.ToHtml());
+            Assert.Null(body.Html.DocumentNode.SelectNodes("//meta"));
+            Assert.Equal("email@domain.com", body.MailTo);
+            Assert.Equal("Activate your account", body.Subject);
+
+        }
+
+    }
+}
---
+++ test/ACs.Net.Mail.Tests/HtmlParameterReplacementTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using ACs.Net.Mail;
+using ACs.Net.Mail.Message;
+using HtmlAgilityPack;
+
+namespace ACs.Net.Mail.Tests
+{
+
+    public class HtmlParameterReplacementTest
+    {
+
+        [Fact]
+        public void ReplaceWithDynamic()
+        {
+            var template = new HtmlDocument();
+            template.LoadHtml("<div><param name=\"url\" /> and <param name=\"another\" />");
+
+            var uri = new Uri("Http://somedomain.com/test");
+            var result = HtmlParameterReplacement.Replace(template, new { url = new Uri("http://somedomain.com/test"), another = "param" });
+            var html = result.DocumentNode.OuterHtml;
+
+            Assert.Null(result.DocumentNode.SelectNodes("//param"));
+            Assert.Contains("<a href", result.DocumentNode.OuterHtml);
+            Assert.Contains("target=\"_blank\"", result.DocumentNode.OuterHtml);
+            Assert.Contains(uri.ToString(), result.DocumentNode.OuterHtml);
+            Assert.Contains("<span>param</span>", result.DocumentNode.OuterHtml);
+        }
+
+        [Fact]
+        public void ReplaceUrl()
+        {
+            var template = new HtmlDocument();
+            template.LoadHtml("<div><param name=\"url\"/></div>");
+
+            var uri = new Uri("Http://somedomain.com/test");
+            var result = HtmlParameterReplacement.Replace(template, "url", uri);
+
+            Assert.Contains("<a href", result.DocumentNode.OuterHtml);
+            Assert.Contains(uri.ToString(), result.DocumentNode.OuterHtml);
+            Assert.Contains("target=\"_blank\"", result.DocumentNode.OuterHtml);
+        }
+
+        [Fact]
+        public void ReplaceString()
+        {
+            var template = new HtmlDocument();
+            template.LoadHtml("<div>Your name is <param name=\"name\"/>.</div>");
+
+            var userName = "User Test";
+            var result = HtmlParameterReplacement.Replace(template, "name", userName);
+
+            Assert.Equal($"<div>Your name is <span>{userName}</span>.</div>", result.DocumentNode.OuterHtml);
+        }
+
+        [Fact]
+        public void ReplaceByDictionary()
+        {
+            var template = new HtmlDocument();
+            template.LoadHtml("<div>Your name is <param name=\"name\"/>.</div>");
+
+            var userName = "User test";
+
+            var result = HtmlParameterReplacement.Replace(template, new Dictionary<string,object>()  {
+                { "name", userName }
+            });
+            
+            Assert.Equal($"<div>Your name is <span>{userName}</span>.</div>", result.DocumentNode.OuterHtml);
+        }
+
+
+
+        [Fact]
+        public void NullParameter()
+        {
+            var template = new HtmlDocument();
+            template.LoadHtml("<div>Your name is <param name=\"name\"/>.</div>");
+
+            
+            var result = HtmlParameterReplacement.Replace(template, new Dictionary<string, object>()  {
+                { "name", null }
+            });
+
+            Assert.Equal("<div>Your name is <span></span>.</div>", result.DocumentNode.OuterHtml);
+        }
+
+
+    }
+}

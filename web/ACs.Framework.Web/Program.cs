fa53c5a843a8f5d143bc15b2eb91155c71db5989
Allan Clempe <allan.clempe@gmail.com>
2017-06-03 10:50:56 +1000

    Upgrading to vs 2017

--- web/ACs.Framework.Web/Program.cs
+++ web/ACs.Framework.Web/Program.cs
@@ -9,16 +9,16 @@ namespace ACs.Framework.Web
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
-			var host = new WebHostBuilder()
-				.UseKestrel()
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.UseIISIntegration()
-				.UseStartup<Startup>()
-				.Build();
-
-			host.Run();
-		}
+//		public static void Main(string[] args)
+//		{
+//			var host = new WebHostBuilder()
+//				.UseKestrel()
+//				.UseContentRoot(Directory.GetCurrentDirectory())
+//				.UseIISIntegration()
+//				.UseStartup<Startup>()
+//				.Build();
+//
+//			host.Run();
+//		}
 	}
 }
